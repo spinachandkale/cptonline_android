24f694e432c5726d49eaa2e1b09b954052803c75
avinashkota <iworkoncomputer@gmail.com>
2017-05-02 15:15:45 -0500

    ...

--- app/src/main/java/edu/ucmo/cptonline/studentInfoActivity.java
+++ app/src/main/java/edu/ucmo/cptonline/studentInfoActivity.java
@@ -1,7 +1,19 @@
 package edu.ucmo.cptonline;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+
+import edu.ucmo.cptonline.datasource.Students;
+import edu.ucmo.cptonline.helper.NetworkRequest;
 
 public class studentInfoActivity extends AppCompatActivity {
 
@@ -10,8 +22,61 @@ public class studentInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_info);
 
+        Button edit = (Button) findViewById(R.id.btnEditStudentInfo);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (((Button)v).getText() == "Edit") {
+                    makeStudentDetailsEditable();
+                } else if(((Button)v).getText() == "Save"){
+                    saveStudentDetails();
+                    proceedToNavigationActivity();
+                }
+            }
+        });
 
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String email = preferences.getString("email", null);
 
+        String[] emailParts = email.split("@");
 
+        NetworkRequest nr = new NetworkRequest("http://35.188.97.91:8761/students/"+emailParts[0]);
+        nr.getRequestDefault();
+        nr.waitForResult();
+        String response = nr.getResponse();
+
+        ObjectMapper mapper = new ObjectMapper();
+        Students[] students;
+        try {
+            students = mapper.readValue(response, Students[].class);
+            if (students.length == 0) {
+                displayFileApplication();
+            } else {
+                displayEditApplication(students[0]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void displayFileApplication() {
+
+    }
+
+    private void displayEditApplication(Students student) {
+
+    }
+
+    private void makeStudentDetailsEditable() {
+
+    }
+
+    private void saveStudentDetails() {
+
+    }
+
+    private void proceedToNavigationActivity() {
+        Intent intent = new Intent(this, StudentNavigationActivity.class);
+        startActivity(intent);
     }
 }
